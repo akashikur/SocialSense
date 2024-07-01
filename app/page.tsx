e21d6bc8b7dbd9238a963b8e4df27ffcf024e3ed
Akash <akashikur011@gmail.com>
2024-07-01 12:59:28 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import HomePage from '../Pages/Home'
-import Navbar from "../components/Navbar"
 const page = () => {
   return (
     <div>
-      <Navbar />
       <HomePage />
     </div>
   )
